628954cc7a01d66b27917e4efaaeaab3bd430ab9
Armando Peralta - XxTavo16xX <tavo14169@gmail.com>
2023-02-10 04:35:46 -0600

    Build: Top Nav Bar Design Complete.

---
+++ src/app/Context.jsx
@@ -0,0 +1,53 @@
+// * Global App Context
+
+// * Dependencies Required
+
+import React from 'react';
+
+// * Creating context
+
+const AppContext = React.createContext();
+
+const defaultContext = {
+    app: {
+        app_name: 'PayAll',
+        current_view: 'Home',
+    },
+    user: {
+        is_session_created: false,
+        user_name: '',
+        user_profile_photo_url: '/src/assets/images/defaultUser.png',
+    }
+}
+
+const AppProvider = (props) => {
+
+    // * Getting context from LocalStorage
+
+    const localContext = localStorage.getItem('localContext')
+
+    // * In case there´s no context app saved into the localStorage the defaultContext will be returned
+
+    if (!localContext) {
+
+        const [context, setContext] = React.useState(defaultContext);
+
+        return (
+            <AppContext.Provider value={{ context, setContext }}>
+
+                {props.children}
+
+            </AppContext.Provider>
+        )
+
+    }
+
+    // * Setting default context
+
+    // ! Must improve context by saving it into the local Storage, in case there´s no data must set the default context
+
+
+
+}
+
+export { AppContext, AppProvider }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
+
+// * Dependencies Required 
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+// * Modules Required
+
+// * view Styles
+
 import './index.css'
+
+// * Components Required
+
+import TopNavBar from './components/shared/TopNavBar'
+
+// * view to Return
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <TopNavBar />
+
   </React.StrictMode>,
 )
